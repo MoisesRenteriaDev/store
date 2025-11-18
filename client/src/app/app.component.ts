ad5b71039183064f316d45e63385cedf32764acd
Joel Moisés Rentería Gallegos <moisesrenteriadev@outlook.com>
2025-11-18 14:46:03 -0600

    shop index page ready

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,26 +1,18 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { HeaderComponent } from './layout/header/header.component';
-import { HttpClient } from '@angular/common/http';
 import { Product } from './shared/models/product';
-import { Pagination } from './shared/models/pagination';
+import { ShopService } from './core/services/shop.service';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppCompoment implements OnInit {
-  baseUrl = 'https://localhost:5001/api/';
-  title = 'Store Index';
-  private http = inject(HttpClient);
-  products: Product[] = [];
 
-  ngOnInit(): void {
-    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-      next: (response) => (this.products = response.data),
-      error: (err) => console.log(err),
-      complete: () => console.log('Requested complete'),
-    });
-  }
+
+export class AppCompoment  {
+  title = 'Skinet'
+
 }
